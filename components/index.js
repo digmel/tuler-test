f7b6612b376629e5bc9d40a67662cf2335b6e749
George <gio.digmel@gmail.com>
2022-08-26 21:11:08 +0400

    chore: create screen component

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export * from "./config/colors";
+export * from "./screen";
 export * from "./header";
 export * from "./heroSection";
---
+++ components/screen/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+import { colors } from "../../components";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    paddingHorizontal: 160,
+  },
+});
