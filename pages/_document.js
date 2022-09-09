0e18a6ed6a24a70e089504494b1b79480c21ee24
George <gio.digmel@gmail.com>
2022-09-08 21:07:04 -0700

    chore: added fonts

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
-import { Children } from 'react'
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { AppRegistry } from 'react-native'
-import config from '../app.json'
+import { Children } from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { AppRegistry } from "react-native";
+import config from "../app.json";
 // Force Next-generated DOM elements to fill their parent's height
 const normalizeNextElements = `
   #__next {
@@ -9,29 +9,36 @@ const normalizeNextElements = `
     flex-direction: column;
     height: 100%;
   }
-`
+`;
 
 export default class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
-    AppRegistry.registerComponent(config.name, () => Main)
-    const { getStyleElement } = AppRegistry.getApplication(config.name)
-    const page = await renderPage()
+    AppRegistry.registerComponent(config.name, () => Main);
+    const { getStyleElement } = AppRegistry.getApplication(config.name);
+    const page = await renderPage();
     const styles = [
       <style dangerouslySetInnerHTML={{ __html: normalizeNextElements }} />,
       getStyleElement(),
-    ]
-    return { ...page, styles: Children.toArray(styles) }
+    ];
+    return { ...page, styles: Children.toArray(styles) };
   }
 
   render() {
     return (
-      <Html style={{ height: '100%' }}>
-        <Head />
-        <body style={{ height: '100%', overflow: 'hidden' }}>
+      <Html style={{ height: "100%" }}>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body style={{ height: "100%", overflow: "hidden" }}>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
