ab4920e87ab4c1cab9fb071a7662688f460ce54e
George <gio.digmel@gmail.com>
2022-09-09 22:55:13 -0700

    fix: font name

--- components/config/textStyles.js
+++ components/config/textStyles.js
@@ -6,6 +6,7 @@ export const textStyles = {
     fontWeight: 600,
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   h1: {
     regular: {
@@ -13,12 +14,14 @@ export const textStyles = {
       fontWeight: 400,
       color: colors.primary,
       lineHeight: "130%",
+      fontFamily: "Poppins",
     },
     bold: {
       fontSize: 64,
       fontWeight: 700,
       color: colors.primary,
       lineHeight: "130%",
+      fontFamily: "Poppins",
     },
   },
   h2: {
@@ -27,24 +30,28 @@ export const textStyles = {
     color: colors.primary,
     lineHeight: "130%",
     marginBottom: 28,
+    fontFamily: "Poppins",
   },
   subtitle: {
     fontSize: 36,
     fontWeight: 300,
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   link: {
     fontSize: 32,
     fontWeight: 700,
     color: colors.link,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   button: {
     fontSize: 32,
     fontWeight: 300,
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   title: {
     fontSize: 24,
@@ -52,6 +59,7 @@ export const textStyles = {
     letterSpacing: "1%",
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   body: {
     fontSize: 20,
@@ -59,22 +67,26 @@ export const textStyles = {
     letterSpacing: "1%",
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   label: {
     fontSize: 20,
     fontWeight: 300,
     letterSpacing: "1%",
     color: colors.grayDark,
+    fontFamily: "Poppins",
   },
   card: {
     fontSize: 32,
     fontWeight: 300,
     color: colors.primary,
     lineHeight: "130%",
+    fontFamily: "Poppins",
   },
   menuItem: {
     fontSize: 24,
     fontWeight: 300,
     color: colors.primary,
+    fontFamily: "Poppins",
   },
 };
