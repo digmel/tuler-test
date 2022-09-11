422d707d5544ea788ea591dd2e79953841feaf13
George <gio.digmel@gmail.com>
2022-09-11 00:57:13 -0700

    chore: added sticky header feature

--- components/screen/index.jsx
+++ components/screen/index.jsx
@@ -5,7 +5,7 @@ import { Header, Footer } from "../../components";
 
 export const Screen = ({ children, isHome, isProject, isAbout }) => {
   return (
-    <ScrollView style={styles.container}>
+    <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
       <Header isHome={isHome} isProject={isProject} isAbout={isAbout} />
 
       <View style={styles.content}>{children}</View>
