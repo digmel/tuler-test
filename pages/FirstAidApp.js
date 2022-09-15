04dba80ca3514b5a73427319addfae500c82a96f
Elene <61632701+lelezedg@users.noreply.github.com>
2022-09-16 00:27:41 +0400

    content

--- pages/FirstAidApp.js
+++ pages/FirstAidApp.js
@@ -70,7 +70,6 @@ export default function GlutenFreeApp() {
       <Section
         content={[
           {            
-            width: 136,
             title: "Persona #1",
           },
         ]}
@@ -81,7 +80,6 @@ export default function GlutenFreeApp() {
       <Section
         content={[
           {            
-            // width: 136,
             title: "Persona #2",
           },
         ]}
