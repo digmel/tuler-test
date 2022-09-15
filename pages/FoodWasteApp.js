2e4cfcf941cdb10c90f3908356d7f3297fcfdc95
Elene <61632701+lelezedg@users.noreply.github.com>
2022-09-15 20:19:30 +0400

    comment

--- pages/FoodWasteApp.js
+++ pages/FoodWasteApp.js
@@ -175,10 +175,10 @@ export default function FoodWasteApp() {
             "“users are able to click the community and see what others are donating.”",
           ],
         },
-        {
-          width: 606,
-          body="In order to encourage first-time users to take action, I've added a microcopy with an arrow and instructions for them to start donating, that’s an empty state.",
-        },           
+        // {
+        //   width: 606,
+        //   body="In order to encourage first-time users to take action, I've added a microcopy with an arrow and instructions for them to start donating, that’s an empty state.",
+        // },           
 
       ]}
       />
