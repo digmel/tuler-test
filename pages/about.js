19844c2b42802612e4c9d8e42de49988b9c4087d
George <gio.digmel@gmail.com>
2022-09-18 16:51:31 -0700

    fix: update about image

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function About() {
           <Section
             content={[
               {
-                width: 825,
+                width: 967,
                 title: "Who is Elene",
                 body: "I'm Elene, UX/UI Designer interested in designing meaningful experiences that shape how we interact with various products or services. Ever since childhood, I’ve wondered why people make the decisions they do, and find it exciting to guess others' emotions, thoughts, or motivations. Furthermore, I've always loved putting myself in someone else's shoes but later realized that it’s a great skill named empathy. That's one of the reasons why UX design is so enjoyable to me.",
               },
@@ -20,7 +20,7 @@ export default function About() {
           <Section
             content={[
               {
-                width: 825,
+                width: 967,
                 title: "Background & BA Degree",
                 body: "The skills and views I’ve gained during my BA (Engineering and Computer Science) are essential to UX design. After graduating, I became interested in graphic design and worked on freelance projects, which led me to UX Design and took the best courses of Google, Idxf, and DesignLab. Completing those comprehensive courses helped me become a confident UX designer.",
               },
@@ -30,7 +30,7 @@ export default function About() {
           <Section
             content={[
               {
-                width: 825,
+                width: 967,
                 title: "Elene in UX Field",
                 body: "I continued my career in UX research, took some courses, did several research projects, and worked as a UX Researcher in the biggest bank of Georgia.\n\nI truly enjoyed being UX Researcher and gained remarkable experience that makes me very comfortable working with other researchers. During that time I realized that I wanted the whole package of UX, and wanted to use my other design skills too. That's how I moved to a design agency as a Product Designer.",
               },
@@ -40,7 +40,7 @@ export default function About() {
           <Section
             content={[
               {
-                width: 825,
+                width: 967,
                 title: "Outside the Work",
                 body: "Outside work, you can find me roller skating! 🛼",
               },
@@ -55,9 +55,9 @@ export default function About() {
 
         <View style={styles.contentEnd}>
           <Image
-            src="/assets/about-cover.png"
-            width={574}
-            height={381}
+            src="/assets/about-cover.jpeg"
+            width={404}
+            height={390}
             alt="Author Image"
           />
         </View>
