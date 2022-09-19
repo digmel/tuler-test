2b73532026c66f828fb520ef044b3dec77b7e14f
George <gio.digmel@gmail.com>
2022-09-18 17:06:26 -0700

    chore: adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import * as React from 'react'
-import Head from 'next/head'
+import * as React from "react";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,9 +8,24 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-87ETS7Y1TK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-87ETS7Y1TK');
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
