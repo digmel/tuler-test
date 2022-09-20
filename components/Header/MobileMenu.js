10fa24bfe669eafae5f2b40dd9b8d524b012f6ec
George <gio.digmel@gmail.com>
2022-09-19 22:40:49 -0700

    fix: add mobile menu navigation

--- components/Header/MobileMenu.js
+++ components/Header/MobileMenu.js
@@ -2,35 +2,72 @@ import { View, Text, Modal } from "react-native";
 import React from "react";
 import { textStyles } from "../config/textStyles";
 import { colors } from "../config/colors";
+import Image from "next/image";
 
 export const MobileMenu = () => {
   return (
     <Modal transparent>
       <View
         style={{
-          width: 300,
+          width: 400,
           zIndex: 10,
           flex: 1,
           backgroundColor: colors.grayHover,
           marginStart: 100,
           paddingVertical: 64,
           paddingStart: 32,
+          borderLeftColor: colors.gray,
+          borderLeftWidth: 1,
         }}
       >
-        <View style={{ marginTop: 12 }} accessibilityRole="link" href={`/`}>
-          <Text style={textStyles.h3}>Home</Text>
+        <View style={{ marginBottom: 12 }} accessibilityRole="link" href={`/`}>
+          <Text style={[textStyles.h3, { fontSize: 20 }]}>Home</Text>
+
+          <View style={{ alignItems: "flex-start", marginStart: -12 }}>
+            <Image src="/assets/active-menu.svg" width={100} height={16} />
+          </View>
         </View>
 
-        <View style={{ marginTop: 12 }}>
-          <Text style={textStyles.h3}>Projects</Text>
+        <View style={{ marginTop: 24 }}>
+          <Text style={[textStyles.h3, { fontSize: 22 }]}>Projects</Text>
         </View>
 
         <View
-          style={{ marginTop: 12 }}
+          style={{ marginTop: 12, marginStart: 16 }}
+          accessibilityRole="link"
+          href={`/GlutenFreeApp`}
+        >
+          <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
+            Gluten Free App
+          </Text>
+        </View>
+
+        <View
+          style={{ marginTop: 32, marginStart: 16 }}
+          accessibilityRole="link"
+          href={`/FoodWasteApp`}
+        >
+          <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
+            Reduce Food Waste App
+          </Text>
+        </View>
+
+        <View
+          style={{ marginTop: 32, marginStart: 16 }}
+          accessibilityRole="link"
+          href={`/FirstAidApp`}
+        >
+          <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
+            Learn First Aid App
+          </Text>
+        </View>
+
+        <View
+          style={{ marginTop: 48 }}
           accessibilityRole="link"
           href={`/about`}
         >
-          <Text style={textStyles.h3}>About</Text>
+          <Text style={[textStyles.h3, { fontSize: 20 }]}>About me</Text>
         </View>
       </View>
     </Modal>
