6ec2fad7e400712974a390a545fbf9ed11cdc3e1
George <gio.digmel@gmail.com>
2022-09-19 22:02:16 -0700

    chore: adding responsive home hero

--- components/HomeHero/index.jsx
+++ components/HomeHero/index.jsx
@@ -1,32 +1,70 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { styles } from "./styles";
 import { textStyles } from "../config/textStyles";
+import { shadowStyle as _shadowStyle } from "../config/shadowStyle";
 import Image from "next/image";
 import { usePlatform } from "../../hooks";
+import { colors } from "..";
 
 export const HomeHero = () => {
   const { isMobile } = usePlatform();
 
-  return (
-    <>
-      {isMobile ? (
-        <View></View>
-      ) : (
-        <View style={styles.container} accessibilityRole="link" href={`/about`}>
-          <View style={styles.contentStart}>
-            <Image src="/assets/author.svg" width={100} height={100} />
-          </View>
+  const containerStyle = {
+    flex: 1,
+    flexDirection: isMobile ? "column" : "row",
+    justifyContent: "flex-start",
+    alignItems: isMobile ? "flex-start" : "center",
+    paddingVertical: isMobile ? 0 : 48,
+    paddingStart: isMobile ? 0 : 32,
+    marginBottom: 48,
+  };
 
-          <View style={styles.contentEnd}>
-            <Text style={textStyles.h3}>Hi, I’m Elene 👋</Text>
-            <Text style={textStyles.subtitle}>
-              Google certified UX/UI Designer with a mission to create
-              delightful experiences and lovable products!
-            </Text>
-          </View>
-        </View>
-      )}
-    </>
+  const shadowStyle = isMobile ? {} : _shadowStyle;
+  const mobileSubtitle = isMobile
+    ? {
+        fontSize: 20,
+        fontWeight: 300,
+        color: colors.primary,
+        lineHeight: "130%",
+        fontFamily: "Poppins",
+        paddingTop: 16,
+      }
+    : {};
+
+  const imageStyle = {
+    alignItems: isMobile ? "center" : "flex-start",
+    justifyContent: "flex-start",
+    paddingRight: isMobile ? 0 : 24,
+    flexDirection: isMobile ? "row" : "column",
+    marginTop: isMobile ? -64 : 0,
+  };
+
+  const contentStyle = {
+    flex: 1,
+  };
+
+  return (
+    <View
+      style={[containerStyle, shadowStyle]}
+      accessibilityRole="link"
+      href={`/about`}
+    >
+      <View style={imageStyle}>
+        <Image src="/assets/author.svg" width={100} height={100} />
+        {isMobile && (
+          <Text style={[textStyles.h3, { marginStart: 16 }]}>
+            Hi, I’m Elene 👋
+          </Text>
+        )}
+      </View>
+
+      <View style={contentStyle}>
+        {!isMobile && <Text style={textStyles.h3}>Hi, I’m Elene 👋</Text>}
+        <Text style={[textStyles.subtitle, mobileSubtitle]}>
+          Google certified UX/UI Designer with a mission to create delightful
+          experiences and lovable products!
+        </Text>
+      </View>
+    </View>
   );
 };
---
+++ components/config/shadowStyle.js
@@ -0,0 +1,9 @@
+import { colors } from "./colors";
+
+export const shadowStyle = {
+  shadowColor: colors.shadow,
+  shadowOffset: { width: 1, height: 1 },
+  shadowOpacity: 0.2,
+  shadowRadius: 7,
+  borderRadius: 24,
+};
