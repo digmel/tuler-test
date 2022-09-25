77865c961adc7ecbc6166f6c600f58fef0a8a6f9
George <gio.digmel@gmail.com>
2022-09-25 01:30:23 -0700

    update mobile menu

--- components/header/MenuMobile.js
+++ components/header/MenuMobile.js
@@ -1,181 +1,134 @@
 import { View, Text, Modal } from "react-native";
 import React from "react";
-import { textStyles, Colors, AnimatedView } from "..";
+import { textStyles, Colors, AnimatedView, constants } from "..";
 import Image from "next/image";
 
 export const MenuMobile = ({ isHome, isProject, isAbout }) => {
+  const containerStyle = {
+    zIndex: 10,
+    flex: 1,
+    backgroundColor: Colors.grayHover,
+    paddingVertical: 24,
+    paddingHorizontal: 32,
+    borderLeftColor: Colors.gray,
+    borderLeftWidth: 1,
+  };
+
+  const menuCloseIconStyle = {
+    marginTop: -15,
+    marginEnd: -22,
+    alignSelf: "flex-end",
+  };
+
+  const contactContainerStyle = {
+    flex: 1,
+    marginTop: 400,
+  };
+
+  const menuItemTextStyle = {
+    fontSize: 20,
+    lineHeight: "130%",
+    fontWeight: 400,
+    color: Colors.primary,
+    fontFamily: "Poppins",
+  };
+
+  const projectItemTextStyle = {
+    fontSize: 16,
+    lineHeight: "130%",
+    fontWeight: 300,
+    color: Colors.primary,
+    fontFamily: "Poppins",
+  };
+
+  const menuItemStyle = {
+    paddingVertical: 36,
+  };
+
+  const projectItemStyle = {
+    flex: 1,
+    paddingVertical: 20,
+    paddingStart: 32,
+    justifyContent: "center",
+    alignItems: "flex-start",
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.accent,
+  };
+
   return (
     <Modal transparent>
       <AnimatedView>
-        <View
-          style={{
-            zIndex: 10,
-            flex: 1,
-            backgroundColor: Colors.grayHover,
-            marginStart: 64,
-            paddingVertical: 64,
-            paddingStart: 32,
-            borderLeftColor: Colors.gray,
-            borderLeftWidth: 1,
-          }}
-        >
-          <View
-            style={{
-              marginTop: -55,
-              marginEnd: 10,
-              alignSelf: "flex-end",
-            }}
-          >
+        <View style={containerStyle}>
+          <View style={menuCloseIconStyle}>
             <Image src="/assets/mobile-menu-close.svg" width={45} height={45} />
           </View>
 
-          <View
-            style={{ marginBottom: 12 }}
-            accessibilityRole="link"
-            href={`/`}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20 }]}>Home</Text>
+          <View style={{ flex: 1 }}>
+            <View style={menuItemStyle} accessibilityRole="link" href={`/`}>
+              <Text style={menuItemTextStyle}>Home</Text>
+            </View>
 
-            {isHome && (
-              <AnimatedView>
-                <View
-                  style={{
-                    alignItems: "flex-start",
-                    marginStart: -12,
-                    marginTop: 4,
-                  }}
-                >
-                  <Image
-                    src="/assets/active-menu.svg"
-                    width={131}
-                    height={16}
-                  />
-                </View>
-              </AnimatedView>
-            )}
-          </View>
+            <View>
+              <Text style={menuItemTextStyle}>Projects</Text>
+            </View>
 
-          <View style={{ marginTop: 24 }}>
-            <Text style={[textStyles.h3, { fontSize: 22 }]}>Projects</Text>
-
-            {isProject && (
-              <AnimatedView>
-                <View
-                  style={{
-                    alignItems: "flex-start",
-                    marginStart: -12,
-                    marginTop: 4,
-                  }}
-                >
-                  <Image
-                    src="/assets/active-menu.svg"
-                    width={131}
-                    height={16}
-                  />
-                </View>
-              </AnimatedView>
-            )}
-          </View>
-
-          <View
-            style={{ marginTop: 16, marginStart: 16 }}
-            accessibilityRole="link"
-            href={`/GlutenFreeApp`}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
-              Gluten Free Finder App
-            </Text>
-          </View>
-
-          <View
-            style={{ marginTop: 32, marginStart: 16 }}
-            accessibilityRole="link"
-            href={`/FoodWasteApp`}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
-              Reduce Food Waste App
-            </Text>
-          </View>
-
-          <View
-            style={{ marginTop: 32, marginStart: 16 }}
-            accessibilityRole="link"
-            href={`/FirstAidApp`}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20, fontWeight: 300 }]}>
-              Learn First Aid App
-            </Text>
-          </View>
-
-          <View
-            style={{ marginTop: 32, marginStart: 16 }}
-            // accessibilityRole="link"
-            // href={`/`}
-          >
-            <Text
-              style={[
-                textStyles.h3,
-                { fontSize: 20, fontWeight: 300, color: "gray" },
-              ]}
+            <View
+              style={projectItemStyle}
+              accessibilityRole="link"
+              href={`/GlutenFreeApp`}
             >
-              Portfolio Website
-            </Text>
-          </View>
+              <Text style={projectItemTextStyle}>Gluten Free Finder App</Text>
+            </View>
 
-          <View
-            style={{ marginTop: 32, marginStart: 16 }}
-            // accessibilityRole="link"
-            // href={`/`}
-          >
-            <Text
-              style={[
-                textStyles.h3,
-                { fontSize: 20, fontWeight: 300, color: "gray" },
-              ]}
+            <View
+              style={projectItemStyle}
+              accessibilityRole="link"
+              href={`/FoodWasteApp`}
             >
-              Startup Info Website
-            </Text>
+              <Text style={projectItemTextStyle}>Reduce Food Waste App</Text>
+            </View>
+
+            <View
+              style={projectItemStyle}
+              accessibilityRole="link"
+              href={`/FirstAidApp`}
+            >
+              <Text style={projectItemTextStyle}>Learn First Aid App</Text>
+            </View>
+
+            <View
+              style={projectItemStyle}
+              // accessibilityRole="link"
+              // href={`/`}
+            >
+              <Text style={[projectItemTextStyle, { color: "gray" }]}>
+                Portfolio Website
+              </Text>
+            </View>
+
+            <View
+              style={projectItemStyle}
+              // accessibilityRole="link"
+              // href={`/`}
+            >
+              <Text style={[projectItemTextStyle, { color: "gray" }]}>
+                Startup Info Website
+              </Text>
+            </View>
+
+            <View
+              style={menuItemStyle}
+              accessibilityRole="link"
+              href={`/about`}
+            >
+              <Text style={menuItemTextStyle}>About me</Text>
+            </View>
           </View>
 
-          <View
-            style={{ marginTop: 48 }}
-            accessibilityRole="link"
-            href={`/about`}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20 }]}>About me</Text>
-
-            {isAbout && (
-              <AnimatedView>
-                <View
-                  style={{
-                    alignItems: "flex-start",
-                    marginStart: -12,
-                    marginTop: 4,
-                  }}
-                >
-                  <Image
-                    src="/assets/active-menu.svg"
-                    width={131}
-                    height={16}
-                  />
-                </View>
-              </AnimatedView>
-            )}
+          <View style={contactContainerStyle}>
+            <Text style={menuItemTextStyle}>Contact</Text>
+            <Text style={textStyles.link}>{constants.socials.Email}</Text>
           </View>
-
-          <View
-            style={{
-              marginTop: 200,
-              borderBottomColor: Colors.grayDark,
-              borderBottomWidth: 0.5,
-              marginEnd: 32,
-              paddingBottom: 4,
-              marginBottom: 12,
-            }}
-          >
-            <Text style={[textStyles.h3, { fontSize: 20 }]}>Contact</Text>
-          </View>
-          <Text style={textStyles.link}>elene.uxdesign@gmail.com</Text>
         </View>
       </AnimatedView>
     </Modal>
