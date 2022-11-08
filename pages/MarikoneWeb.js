e9fa7f09d991b13498ceb10d81cb0f2374389518
George <gio.digmel@gmail.com>
2022-11-07 19:25:29 -0800

    chore: added GIF support

--- pages/MarikoneWeb.js
+++ pages/MarikoneWeb.js
@@ -6,7 +6,7 @@ export default function Page({ isMobile, constants }) {
       <SectionHero
         isMobile={isMobile}
         link={constants.links.FirstAidApp}
-        // image="marikone-large-hero"
+        image="marikone-hero"
         headline="Fashion Designer’s Portfolio"
         label="Fashion designer's Responsive website, where people can find her collection and contact details."
       />
@@ -25,7 +25,6 @@ export default function Page({ isMobile, constants }) {
           {
             subtitle: "My Role & Project Duration",
             list: [
-
               {
                 icon: "section-icon.svg",
                 text: "UX/UI Designer",
@@ -34,12 +33,11 @@ export default function Page({ isMobile, constants }) {
                 // icon: "section-icon.svg",
                 text: "1 Week",
               },
-
             ],
           },
         ]}
       />
-           <Section
+      <Section
         title="Wireframing"
         isMobile={isMobile}
         content={[
@@ -48,7 +46,7 @@ export default function Page({ isMobile, constants }) {
             text: "During the wireframing, I created multiple sketches and iterations in order to define the best possible layout for the website. The goal was to design a simple page with the most important details in mind, such as, people should immediately recognize that this is a fashion designer’s portfolio website and show the latest designs of Marikone, little information about the author, popular products, and contact form. Here you can see the best option that I chose.",
           },
           {
-            image: "marikone-basic-wireframes",
+            imageGIF: "marikone-basic-wireframes",
             style: "end",
           },
         ]}
@@ -69,13 +67,13 @@ export default function Page({ isMobile, constants }) {
           },
         ]}
       />
-           <Section
+      <Section
         title="Design and Reasoning"
         isMobile={isMobile}
         content={[
           {
             subtitle: "Introduction",
-            text: "For a portfolio website, it's essential to immediately let the people know what kind of website are they visiting. I choose to keep it simple and do it with a minimalistic title and make users interested by showing Marikone's latest collections. The collection section will always be updated with her recent work and people will be able to browse what kind of clothes Marikone creates.",
+            text: "For a portfolio website, it's essential to immediately let the people know what kind of website are they visiting. I choose to keep it simple and do it with a minimalist title and make users interested by showing Marikone's latest collections. The collection section will always be updated with her recent work and people will be able to browse what kind of clothes Marikone creates.",
           },
           {
             image: "marikone-introduction",
@@ -83,7 +81,7 @@ export default function Page({ isMobile, constants }) {
           },
         ]}
       />
-           <Section
+      <Section
         isMobile={isMobile}
         variant="isMobile-reverse"
         topSpace={isMobile ? -80 : -100}
