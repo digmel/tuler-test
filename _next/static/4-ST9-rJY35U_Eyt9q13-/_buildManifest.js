4786ebb06dbad1af0813412a3916f06d7379f232
digmel <47932883+digmel@users.noreply.github.com>
2023-01-21 03:05:17 +0000

    Deploying to public from @ digmel/tuler-test@afdd0fa6342bf65a69a356fdab0a1d9b9c5ea4b3 🚀

--- _next/static/4-ST9-rJY35U_Eyt9q13-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/482-4e3e920eb6fc3cf1.js","static/chunks/pages/index-61a0ec0889da44a9.js"],"/_error":["static/chunks/pages/_error-28b0dba9dbcfb4ed.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
